852a638d9e5fd6d8963100968522779de9bc9206
Eric <46439307+EricTheDevil@users.noreply.github.com>
2025-02-19 14:40:44 +0200

    Update hours Contact.js

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -8,13 +8,13 @@ function Contact() {
 					<div id="itemContact">
 						<h1> Openingsuren </h1>
 						<ul>
-							<li>Maandag : 17:00 - 21:30 </li>
-							<li>Dinsdag : 17:00 - 21:30 </li>
+							<li>Maandag : 17:00 - 21:00 </li>
+							<li>Dinsdag : 17:00 - 21:00 </li>
 							<li>Woensdag : Gesloten </li>
-							<li>Donderdag : 17:00 - 21:30 </li>
-							<li>Vrijdag : 17:00 - 21:30 </li>
-							<li>Zaterdag : 17:00 - 21:30 </li>
-							<li>Zondag : 12:00 - 22:00</li>
+							<li>Donderdag : 17:00 - 21:00 </li>
+							<li>Vrijdag : 17:00 - 21:00 </li>
+							<li>Zaterdag : 17:00 - 21:00 </li>
+							<li>Zondag : 12:00 - 14:00 en 17:00 - 21:00</li>
 						</ul>
 						<h1> Telefoon </h1>
 						<ul>
